f7c3536ebfb09f011ffc69e92641ce07c4be0f0c
devhasanmia <dev.hasanmia@gmail.com>
2025-08-30 23:52:39 +0600

    schedule update

--- src/app/modules/classSchedule/classSchedule.model.ts
+++ src/app/modules/classSchedule/classSchedule.model.ts
@@ -1,16 +1,16 @@
 import { model, Schema } from "mongoose";
-import { Color, Day, IClassSchedule } from "./classSchedule.interface";
+import { IClassSchedule } from "./classSchedule.interface";
 
 const ClassScheduleSchema = new Schema<IClassSchedule>(
   {
     user: { type: Schema.Types.ObjectId, ref: "User" },
     subject: { type: String, required: true },
     instructor: { type: String, required: true },
-    day: { type: String, enum: Object.values(Day), required: true },
+    day: { type: String, required: true },
     startTime: { type: String, required: true },
     endTime: { type: String, required: true },
     location: { type: String },
-    color: { type: String, enum: Object.values(Color), default: "#3B82F6"},
+    color: { type: String, default: "#3B82F6" },
   },
   { timestamps: true }
 );
--- src/app/modules/classSchedule/classSchedule.validattion.ts
+++ src/app/modules/classSchedule/classSchedule.validattion.ts
@@ -4,7 +4,6 @@ const DayEnum = z.enum(["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
 const ColorEnum = z.enum([
     "#3B82F6", "#10B981", "#F59E0B", "#EF4444", "#8B5CF6", "#06B6D4", "#F97316"
 ]).optional();
-const timeFormatRegex = /^(0?[1-9]|1[0-2]):[0-5][0-9] (AM|PM)$/;
 
 
 
@@ -29,12 +28,10 @@ export const classScheduleUpdateValidationSchema = z.object({
     day: DayEnum,
     startTime: z
         .string({ error: "Start time is required" })
-        .min(1, { error: "Start time is required" })
-        .regex(timeFormatRegex, { error: "Start time must be in 12:30 PM format" }).optional(),
+        .min(1, { error: "Start time is required" }).optional(),
     endTime: z
         .string({ error: "End time is required" })
-        .min(1, { error: "End time is required" })
-        .regex(timeFormatRegex, { error: "End time must be in 12:30 PM format" }).optional(),
+        .min(1, { error: "End time is required" }).optional(),
     location: z.string().optional(),
     color: ColorEnum
 });
