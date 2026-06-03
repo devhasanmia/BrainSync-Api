1296ce74ff7b1d97c3803af615760d64e8d9da1e
MD HASAN MIA <150786473+devhasanmia@users.noreply.github.com>
2026-06-03 23:41:09 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://brain-sync-alpha.vercel.app",
+    origin: "*",
     credentials: true,
   })
 );
